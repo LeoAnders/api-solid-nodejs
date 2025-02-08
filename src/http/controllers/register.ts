50a31e71b9c7262dc698b6b4b2b36c97e8e9e286
LeoAnders <leonardoanders15@gmail.com>
2025-02-08 20:24:56 -0300

    implementar controller de autenticação

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,8 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
-import { RegisterUseCase } from '@/user-cases/register'
+import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { UserAlreadyExistsError } from '@/user-cases/erros/user-already-exists-error'
+import { UserAlreadyExistsError } from '@/use-cases/erros/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
